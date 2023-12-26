069c47ff73e4ecdf64b3844559004e73a2c689fc
mikkisy <154649587+mikkisy@users.noreply.github.com>
2023-12-27 01:12:21 +0300

    11.jl

--- 11.jl
+++ 11.jl
@@ -1,37 +1,46 @@
-using HorizonSideRobots
-
-inverse( side :: HorizonSide) = HorizonSide(mod(Int(side)+2,4))
-
-function collibrate!( r :: Robot; track=false )
-    arr=()
-    while !isborder(r,Sud) || !isborder(r,West)
-        if !isborder(r,Sud) move!(r,Sud); arr=(Nord,arr...) end
-        if !isborder(r,West) move!(r,West); arr=(Ost,arr...) end
+function move_two_sides(robot, x, y):nothing
+    while y < 0
+        move!(robot, Nord)
+        y += 1
     end
-    if track return arr end
+    while x > 0
+        move!(robot, West)
+        x -= 1
+    end
 end
-
-gohome!(r :: Robot, arr :: NTuple)=for side in arr move!(r,side) end
-
-function go_count!( r :: Robot, side :: HorizonSide)
-    flag=false; c=0
-    while !isborder(r,side)
-        if isborder(r,Nord)
-            if !flag flag=true; c+=1 end
-        else
-            flag=false
+ 
+function result(robot):nothing
+    x = 0
+    y = 0
+    while !isborder(robot, West)
+        move!(robot, West)
+        x -= 1
+    end
+    while !isborder(robot, Nord)
+        move!(robot, Nord)
+        y += 1
+    end
+    cnt = 0
+    dir = Ost
+    while (!isborder(robot, Ost) || !isborder(robot, Sud))
+        flag = false
+        while !isborder(robot, dir)
+            move!(robot, dir)
+            if dir == Ost x += 1
+            else x -= 1 end
+            if isborder(robot, Sud) flag = true
+            else 
+                if flag cnt += 1 end 
+                flag = false 
+            end
         end
-        move!(r,side)
+        if !isborder(robot, Sud)
+            move!(robot, Sud)
+            y -= 1
+        end
+        if dir == Ost dir = West
+        else dir = Ost end
     end
-    return c
-end
-
-function countbords!(r :: Robot)
-    wayhome=collibrate!(r; track=true); side=Ost; c=0
-    while !isborder(r,Nord) && !isborder(r,side)
-        c+=go_count!(r,side); side=inverse(side);move!(r,Nord)
-    end
-    collibrate!(r)
-    gohome!(r,wayhome)
-    println(c)
+    print(cnt)
+    move_two_sides(robot, x, y)
 end
