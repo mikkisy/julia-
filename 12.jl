f78fc8f3b1b6406a65b9dff38c3f08c0bb2454c6
mikkisy <154649587+mikkisy@users.noreply.github.com>
2023-12-27 01:14:42 +0300

    Update 12.jl

--- 12.jl
+++ 12.jl
@@ -1,25 +1,54 @@
-# isborder = stop_condition()
-function move_by_steps(r, Side, Numsteps)
-    while Numsteps != 0
-        move!(r, Side)
-        Numsteps -= 1
+function move_two_sides(robot, x, y):nothing
+    while y < 0
+        move!(robot, Nord)
+        y += 1
+    end
+    while x > 0
+        move!(robot, West)
+        x -= 1
     end
 end
-
-function shuttle!(stop_condition::Function, r, side)
-    side1 = Ost
-    side2 = West
-    a = 0
-    b = 1
-    while stop_condition(r, side) == 1 
-        if a % 2 == 0
-            move_by_steps!(r, side1, b)
-        end
-        if a % 2 == 1
-            move_by_steps!(r, side2, b)
-        end
-        b += 1
-        a += 1
+ 
+function result(robot):nothing
+    x = 0
+    y = 0
+    while !isborder(robot, West)
+        move!(robot, West)
+        x -= 1
     end
-    move!(r, side)
+    while !isborder(robot, Nord)
+        move!(robot, Nord)
+        y += 1
+    end
+    cnt = 0
+    dir = Ost
+    flag = false
+    while (!isborder(robot, Ost) || !isborder(robot, Sud))
+        if flag cnt += 1 end
+        flag = false
+        delay = 0
+        while !isborder(robot, dir)
+            move!(robot, dir)
+            if dir == Ost x += 1
+            else x -= 1 end
+            if isborder(robot, Sud)
+                flag = true
+                delay = 0
+            else 
+                delay += 1
+                if (flag && delay > 1) 
+                    cnt += 1
+                    flag = false 
+                end 
+            end
+        end
+        if !isborder(robot, Sud)
+            move!(robot, Sud)
+            y -= 1
+        end
+        if dir == Ost dir = West
+        else dir = Ost end
+    end
+    print(cnt)
+    move_two_sides(robot, x, y)
 end
