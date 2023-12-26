2c730b4ed1022d373e62dc2c034718ff3779c581
mikkisy <154649587+mikkisy@users.noreply.github.com>
2023-12-27 01:15:19 +0300

    Update 16.jl

--- 16.jl
+++ 16.jl
@@ -1,7 +1,25 @@
-using HorizonSideRobots
-include("../RobotHell.jl")
+# isborder = stop_condition()
+function move_by_steps(r, Side, Numsteps)
+    while Numsteps != 0
+        move!(r, Side)
+        Numsteps -= 1
+    end
+end
 
-function find_marker!(r :: Robot)
-    base=BaseRobot(r)
-    spiral!((x...)->!ismarker(base),base)
+function shuttle!(stop_condition::Function, r, side)
+    side1 = Ost
+    side2 = West
+    a = 0
+    b = 1
+    while stop_condition(r, side) == 1 
+        if a % 2 == 0
+            move_by_steps!(r, side1, b)
+        end
+        if a % 2 == 1
+            move_by_steps!(r, side2, b)
+        end
+        b += 1
+        a += 1
+    end
+    move!(r, side)
 end
