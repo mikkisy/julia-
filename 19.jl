8cf2e6066b628fbb6401a6ad8974951442750627
mikkisy <154649587+mikkisy@users.noreply.github.com>
2023-12-27 01:16:36 +0300

    Update 19.jl

--- 19.jl
+++ 19.jl
@@ -1,13 +1,6 @@
-using HorizonSideRobots
-
-inverse( side :: HorizonSide) = HorizonSide(mod(Int(side)+2,4))
-
-function goandback!(r :: Robot, side :: HorizonSide) 
-    if isborder(r,side) 
-        putmarker!(r) 
-    else
-        move!(r,side)
-        goandback!(r, side)
-        move!(r,inverse(side))
+function along_rec!(robot, side)
+    if !isborder(robot, side)
+        move!(robot, side)
+        along_rec!(robot, side)
     end
 end
