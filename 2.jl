899fc006e998108b1d57663b0e1d8b2750676edf
mikkisy <154649587+mikkisy@users.noreply.github.com>
2023-12-27 01:17:05 +0300

    Update 2.jl

--- 2.jl
+++ 2.jl
@@ -1,10 +1,36 @@
-using HorizonSideRobots
-include("../RobotHell.jl")
-
-function perimetr!(r :: Union{SampleRobot,Robot})
-    wayhome= collibrate!(r)
-    for side in (Ost,Sud,West,Nord)
-        along!((x...)->!isborder(r,side), Painter( r ), side)
+function goandmark!(r, Side)
+    while !isborder(r, Side)
+        putmarker!(r)
+        move!(r, Side)
     end
-    gohome!(r,wayhome)
+end
+ 
+ 
+function task2!(r, Side1, Side2, Side3, Side4)
+	Side1 = Nord
+	Side2 = Ost
+	Side3 = Sud 
+	Side4 = West
+    a = 0
+    b = 0
+    while !isborder(r, Side1)
+        move!(r, Side1)
+        a += 1
+    end
+    while !isborder(r, Side2)
+        move!(r, Side2)
+        b += 1
+    end
+    goandmark!(r, Side3)
+    goandmark!(r, Side4)
+    goandmark!(r, Side1)
+    goandmark!(r, Side2)
+    while a != 0
+        move!(r, Side3)
+        a -= 1
+    end
+    while b != 0
+        move!(r, Side4)
+        b -= 1
+    end
 end
