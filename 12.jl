ff7d185501fc96773667b9e27f967b57ef43c090
mikkisy <154649587+mikkisy@users.noreply.github.com>
2023-12-27 01:14:11 +0300

    Update 12.jl

--- 12.jl
+++ 12.jl
@@ -1,40 +1,25 @@
-using HorizonSideRobots
-
-inverse( side :: HorizonSide) = HorizonSide(mod(Int(side)+2,4))
-
-function collibrate!( r :: Robot; track=false )
-    arr=()
-    while !isborder(r,Sud) || !isborder(r,West)
-        if !isborder(r,Sud) move!(r,Sud); arr=(Nord,arr...) end
-        if !isborder(r,West) move!(r,West); arr=(Ost,arr...) end
+# isborder = stop_condition()
+function move_by_steps(r, Side, Numsteps)
+    while Numsteps != 0
+        move!(r, Side)
+        Numsteps -= 1
     end
-    if track return arr end
 end
 
-gohome!(r :: Robot, arr :: NTuple)=for side in arr move!(r,side) end
-
-function go_count!( r :: Robot, side :: HorizonSide)
-    flag=falses(2); c=0
-    while !isborder(r,side)
-        if isborder(r,Nord)
-            if !(flag[1] || flag[2]) c+=1 end
-            flag=trues(2)
-        else
-            if flag[1] flag[1]=false 
-            else flag[2]=false end
+function shuttle!(stop_condition::Function, r, side)
+    side1 = Ost
+    side2 = West
+    a = 0
+    b = 1
+    while stop_condition(r, side) == 1 
+        if a % 2 == 0
+            move_by_steps!(r, side1, b)
         end
-        move!(r,side)
+        if a % 2 == 1
+            move_by_steps!(r, side2, b)
+        end
+        b += 1
+        a += 1
     end
-    return c
-end
-
-function countbords!(r :: Robot)
-    wayhome=collibrate!(r; track=true); side=Ost; c=0
-    while !isborder(r,Nord) && !isborder(r,side)
-        c+=go_count!(r,side); side=inverse(side);move!(r,Nord)
-        println("c: ", c)
-    end
-    collibrate!(r)
-    gohome!(r,wayhome)
-    println(c)
+    move!(r, side)
 end
