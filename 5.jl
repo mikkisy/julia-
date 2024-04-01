78ab195139ad8319882966e7ef4b0fad2a66b4e0
mikkisy <154649587+mikkisy@users.noreply.github.com>
2024-04-01 11:02:10 +0300

    Update 5.jl

--- 5.jl
+++ 5.jl
@@ -1,62 +1,62 @@
-using HorizonSideRobots
-include("../RobotHell.jl")
-include("../Exercise 2/perimetr.jl")
-
-function go!( cond :: Function, r :: Robot, side :: HorizonSide, steps :: Int)
-    while steps>0
-        if cond(r, side) return (true, side) end
-        move!(r,side); steps-=1
+function along!(robot, direct)::Nothing
+    while !isborder(robot, direct)
+        move!(robot, direct)
     end
-    return (false,side) 
 end
 
-
-function count_dimension!(r :: Robot, side :: HorizonSide)
-    steps=0
-    while !isborder(r,side)
-        move!(r,side)
-        steps+=1
+function num_steps_along!(robot, direct)::Int
+    num_steps = 0
+    while !isborder(robot, direct)
+        move!(robot, direct)
+        num_steps += 1
     end
-    return steps
+    return num_steps
 end
 
-
-function close_borders(r :: Robot, side :: HorizonSide, x :: Int, y :: Int)
-    for i in 1:4
-        res=go!((r,side)->isborder(r,anticlockwise(side)),r,side ,x*mod(i,2)+y*mod(i+1,2))
-        if res[1]==true return res end
-        side=anticlockwise(side)
+function put_markers_along!(robot, direct)::Nothing
+    while !isborder(robot, direct)
+        move!(robot, direct)
+        putmarker!(robot)
     end
-    return (false,Nord)
 end
 
-function find_innerper(r :: Robot)
-    collibrate!(r;track=false); side=Ost; flag=()
-    x=count_dimension!(r,Ost); collibrate!(r;track=false)
-    y=count_dimension!(r,Sud); collibrate!(r;track=false)
+function task5!(robot)::Nothing
+    sides = [Ost, Sud, West, Nord]
+    stepsNord = num_steps_along!(robot, Nord)
+    stepsWest = num_steps_along!(robot, West)
+    putmarker!(robot)
+    for i in sides
+        put_markers_along!(robot, i)
+    end
+    sides = [Ost, West]
+    i = 1
     while true
-        flag=close_borders(r,side,x,y)
-        if flag[1]==true break end
-        x-=2; y-=2
-        move!(r,Sud); move!(r,Ost)
+        move!(robot, sides[i])
+        if isborder(robot, sides[i])
+            if !ismarker(robot)
+                move!(robot, Nord)
+                putmarker!(robot)
+                sides = [West, Sud, Ost, Nord]
+                for i in 1:4
+                    move!(robot, sides[i])
+                    while isborder(robot, sides[i % 4 + 1])
+                        putmarker!(robot)
+                        move!(robot, sides[i])
+                    end
+                    putmarker!(robot)
+                end
+                break
+            end
+            move!(robot, Sud)
+            i = i % 2 + 1
+        end
+    end          
+    along!(robot, Nord)
+    along!(robot, West)
+    for i in 1:stepsNord
+        move!(robot, Sud)
     end
-    return flag[2]
-end
-
-function fill_innerper(r :: Robot, side :: HorizonSide)
-    for i in 1:4
-        along!((r,side)->isborder(r,anticlockwise(side)), Painter(r), side)
-        side=anticlockwise(side)
-        move!(r,side)
+    for i in 1:stepsWest
+        move!(robot, Ost)
     end
 end
-
-function fill_all_pers!(r :: Robot)
-    wayhome=collibrate!(r)
-    perimetr!(r); collibrate!(r;track=false)
-    side=find_innerper(r)
-    println(side)
-    fill_innerper(r,side)
-    collibrate!(r; track=false); gohome!(r,wayhome)
-end
-
