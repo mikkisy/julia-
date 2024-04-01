bb7588b3ee309a0d2c700b934429d1b63cc03cec
mikkisy <154649587+mikkisy@users.noreply.github.com>
2024-04-01 11:03:41 +0300

    Update 7.jl

--- 7.jl
+++ 7.jl
@@ -1,18 +1,19 @@
-using HorizonSideRobots
-
-inverse(side :: HorizonSide) = HorizonSide(mod(Int(side)+2,4))
-
-
-function under_gap!(r :: Robot)
-    steps=1
-    side=West
-    while isborder(r,Nord)
-        step=steps
-        while !isborder(r,side) && step>0 && isborder(r,Nord)
-            move!(r,side)
-            step-=1
-        end
-        side=inverse(side)
-        steps+=1
+function move_by_steps(r, Side, Numsteps)
+    while Numsteps != 0
+        move!(r, Side)
+        Numsteps -= 1
     end
+end
+function task7!(r, Prohod)
+    i = 1
+    while isborder(r, Prohod)
+        if i % 2 == 1
+            move_by_steps(r, Ost, i)
+            i += 1
+        else
+            move_by_steps(r, West, i)
+            i += 1
+        end
+    end
+    move!(r, Prohod)
 end
