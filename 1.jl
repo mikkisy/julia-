a7096d63fc69701033231c0f16a65af8e97e612f
mikkisy <154649587+mikkisy@users.noreply.github.com>
2024-05-29 23:51:19 +0300

    Update 1.jl

--- 1.jl
+++ 1.jl
@@ -1,32 +1,30 @@
-function result!(robot):Nothing
-    a = 0
-    putmarker!(robot)
-    while !isborder(robot, Nord)
-        move!(robot, Nord)
-        putmarker!(robot)
-        a -= 1
+using HorizonSideRobots
+
+r=Robot(15,15; animate=true)
+
+#ЭТО КРЕСТИК СТРОИТ
+function mark_kross(r::Robot) 
+    for side in (HorizonSide(i) for i=0:3)
+        putmarkers!(r,side)
+        move_by_markers(r,inverse(side))
     end
-    while !isborder(robot, Sud)
-        move!(robot, Sud)
-        putmarker!(robot)
-        a += 1
-    end
-    while a > 0
-        move!(robot, Nord)
-        a -= 1
-    end
-    while !isborder(robot, West)
-        move!(robot, West)
-        putmarker!(robot)
-        a -= 1
-    end
-    while !isborder(robot, Ost)
-        move!(robot, Ost)
-        putmarker!(robot)
-        a += 1
-    end
-    while a > 0
-        move!(robot, West)
-        a -= 1
+    putmarker!(r)
+end
+putmarkers!(r::Robot,side::HorizonSide) = 
+while isborder(r,side)==false 
+    move!(r,side)
+    putmarker!(r)
+end
+move_by_markers(r::Robot,side::HorizonSide) = 
+while ismarker(r)==true 
+    move!(r,side) 
+end
+inverse(side::HorizonSide) = HorizonSide(mod(Int(side)+2, 4)) 
+function gocentre(r::Robot)
+    for i in (1:7)
+    move!(r,HorizonSide(3))
+    move!(r,HorizonSide(0))
     end
 end
+gocentre(r)
+mark_kross(r)
