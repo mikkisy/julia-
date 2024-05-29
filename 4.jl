cef7ff82787e7d2f3ae510496fa0b5b61debeb1a
mikkisy <154649587+mikkisy@users.noreply.github.com>
2024-05-29 23:50:30 +0300

    Update 4.jl

--- 4.jl
+++ 4.jl
@@ -1,25 +1,24 @@
-function dmove!(r, side1, side2)
-    move!(r, side1)
-    move!(r, side2)
-end 
-function diagonalalongandback!(r, Side1, Side2, Side3, Side4)
-    while !isborder(r, Side1) && !isborder(r, Side2)
-        dmove!(r, Side1, Side2)
+using HorizonSideRobots
+
+r=Robot(15,15; animate=true)
+
+#ЭТО ЛЕСЕНКУ СТРОИТЬ ИСКОСОКОВУЮ
+
+function build_lesenka(r::Robot)
+    while isborder(r,HorizonSide(3))==false
+        putmarker!(r)
+        move!(r,HorizonSide(3))
+        move!(r,HorizonSide(0))
+    end
+    putmarker!(r)
+    while isborder(r,HorizonSide(2))==false
+        move!(r,HorizonSide(2))
+    end
+    putmarker!(r)
+    while isborder(r,HorizonSide(1))==false
+        move!(r,HorizonSide(1))
+        move!(r,HorizonSide(0))
         putmarker!(r)
     end
-    while ismarker(r) == 1
-        dmove!(r, Side3, Side4)
-    end
 end
- 
-function task4!(r)
-	Side1 = Nord
-	Side2 = Ost
-	Side3 = Sud 
-	Side4 = West
-    diagonalalongandback!(r, Side1, Side2, Side3, Side4)
-    diagonalalongandback!(r, Side2, Side3, Side4, Side1)
-    diagonalalongandback!(r, Side3, Side4, Side1, Side2)
-    diagonalalongandback!(r, Side4, Side1, Side2, Side3)
-    putmarker!(r)   
-end
+build_lesenka(r)
