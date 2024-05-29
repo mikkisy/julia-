2139f452a873c1a667020b2eebda4693935798db
mikkisy <154649587+mikkisy@users.noreply.github.com>
2024-05-29 23:38:51 +0300

    Update 7.jl

--- 7.jl
+++ 7.jl
@@ -1,19 +1,21 @@
-function move_by_steps(r, Side, Numsteps)
-    while Numsteps != 0
-        move!(r, Side)
-        Numsteps -= 1
+function iceout!(robot,side)
+    side1 = side
+    side_for_move = right(side1)
+    n = 0
+    while isborder(robot,side1)
+        n+=1
+        along!(robot,side_for_move,n)
+        side_for_move = inverse(side_for_move)
     end
 end
-function task7!(r, Prohod)
-    i = 1
-    while isborder(r, Prohod)
-        if i % 2 == 1
-            move_by_steps(r, Ost, i)
-            i += 1
-        else
-            move_by_steps(r, West, i)
-            i += 1
-        end
+
+function along!(robot,side,n_steps)
+    for _ in 1:n_steps
+        move!(robot,side)
     end
-    move!(r, Prohod)
 end
+
+
+left(side::HorizonSide)::HorizonSide = HorizonSide(mod(Int(side) + 3 , 4)) 
+right(side::HorizonSide)::HorizonSide = HorizonSide(mod(Int(side) + 1 , 4)) 
+inverse(side::HorizonSide)::HorizonSide = HorizonSide(mod(Int(side) + 2 , 4))
