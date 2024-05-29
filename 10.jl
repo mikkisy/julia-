c5e5f2cb482a8e9dff0ddf326c23b7838986f9f6
mikkisy <154649587+mikkisy@users.noreply.github.com>
2024-05-29 23:52:18 +0300

    Update 10.jl

--- 10.jl
+++ 10.jl
@@ -1,54 +1,62 @@
-function check(robot, x, y, r)
-    fl1 = (x % (2 * r) < r)
-    fl2 = (abs(y) % (2 * r) < r)
-    if fl1 == fl2
-        putmarker!(robot)
+function staggered_order_marker!(robot)
+    imax, jmax = r.situation.frame_size
+    num_Sud, num_West = to_corner!(robot)
+    if (num_Sud + num_West) % 2 == 0
+        staggered_order = 0
+    else
+        staggered_order = 1
     end
+    side = Ost
+    while !isborder(robot, side)
+        if(staggered_order % 2 == 0)
+            putmarker!(robot)
+        end
+        move!(robot, side)
+        staggered_order += 1
+        if(isborder(robot, side) && !isborder(robot, Nord))
+            if(staggered_order % 2 == 0)
+                putmarker!(robot)
+            end
+            move!(robot, Nord)
+            staggered_order += 1
+            if(staggered_order % 2 == 0)
+                putmarker!(robot)
+            end
+            side = inverse(side)
+        end
+    end
+    putmarker!(robot)
+    if jmax % 2 == 0
+        num_West = jmax - 1 - num_West
+        side_back = West
+    else
+        side_back = Ost
+    end
+    num_Sud = imax - 1 - num_Sud
+    along!(robot, num_Sud, num_West, side_back)
 end
- 
-function go_back(robot, x, y)
-    while x > 0
-        move!(robot, West)
-        x -= 1
+
+function to_corner!(robot)
+    num_West = 0
+    num_Sud = 0
+    while !isborder(robot, Sud)
+        move!(robot, Sud)
+        num_Sud += 1
     end
-    while y < 0
-        move!(robot, Nord)
-        y += 1
-    end
-end
- 
-function result(robot, r)
-    x = 0
-    y = 0
     while !isborder(robot, West)
         move!(robot, West)
+        num_West += 1
     end
-    while !isborder(robot, Nord)
-        move!(robot, Nord)
-    end
- 
-    check(robot, x, y, r)
-    dir = Ost
-    while (!isborder(robot, Ost) || !isborder(robot, Sud))
-        while !isborder(robot, dir)
-            move!(robot, dir)
-            if dir == Ost
-                x += 1
-            else
-                x -= 1
-            end
-            check(robot, x, y, r)
-        end
-        if !isborder(robot, Sud)
-            move!(robot, Sud)
-            y -= 1
-            check(robot, x, y, r)
-        end
-        if dir == Ost
-            dir = West
-        else
-            dir = Ost
-        end
-    end
-    go_back(robot, x, y)
+    return num_Sud, num_West
 end
+
+function along!(robot, num_Sud, num_Ost, side_back)
+    for _i in 1:num_Sud
+        move!(robot, Sud)
+    end
+    for _i in 1:num_Ost
+        move!(robot,side_back)
+    end
+end
+
+inverse(side::HorizonSide) = HorizonSide((Int(side) +2)% 4)
